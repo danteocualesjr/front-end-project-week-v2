36a1b2157e650d6b3220b9ebd3f7fd712a887ff4
Dante O. Cuales, Jr <danteocualesjr@gmail.com>
2018-06-24 07:51:21 +0800

    Configured index.js, wrapped App with Provider and Router.

--- lambda-notes/src/index.js
+++ lambda-notes/src/index.js
@@ -1,8 +1,27 @@
+// React imports
 import React from 'react';
 import ReactDOM from 'react-dom';
+// Component imports
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+// Router imports
+import { BrowserRouter as Router } from "react-router-dom";
+// Redux imports
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const dummyReducer = (state = [], action) => state;
+
+const store = createStore(dummyReducer);
+
+ReactDOM.render(
+    (
+        <Provider store={store}>
+            <Router>
+                <App />
+            </Router>
+        </Provider>
+    )
+    , document.getElementById('root'));
 registerServiceWorker();
