733b544cbe4eaaa283c2e12f583823418823f0fd
Dante O. Cuales, Jr <danteocualesjr@gmail.com>
2018-06-24 08:41:25 +0800

    Created folders and files for each component: 1. CreateNote 2. DeleteNote 3. EditNote 4. LeftNav 5. MainContent 6. NotesList 7. NoteView

---
+++ lambda-notes/src/components/LeftNav/LeftNav.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './index.css';
+
+class LeftNav extends Component {
+    render() {
+        return (
+            <div>
+                LeftNav placeholder...
+            </div>
+        )
+    }
+}
+
+export default LeftNav;
