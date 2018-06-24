835e2acb1298a7a63b339ba15455bf39607dfc1e
Dante O. Cuales, Jr <danteocualesjr@gmail.com>
2018-06-24 20:53:43 +0800

    Styled LeftNav bar/component.

--- lambda-notes/src/components/LeftNav/LeftNav.js
+++ lambda-notes/src/components/LeftNav/LeftNav.js
@@ -5,7 +5,13 @@ class LeftNav extends Component {
     render() {
         return (
             <div className="left-nav-container">
-                Placeholder for LeftNav...
+                <div>
+                    <h1 className="left-nav-header">Lambda Notes</h1>
+                </div>
+                <div className="left-nav-buttons-container">
+                    <div className="left-nav-button">View Your Notes</div>
+                    <div className="left-nav-button">+ Create New Note</div>
+                </div>
             </div>
         )
     }
